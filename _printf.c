7c06b9467e9486ed02c6b38934d7c8168a6578f8
Trmrskr <simonalale@gmail.com>
2022-08-08 04:26:14 -0700

    printf function draft

---
+++ _printf.c
@@ -0,0 +1,39 @@
+#include <stdargs.h>
+#include "main.h"
+/**
+ * _printf - clone of the c printf standard library function
+ * @format: the string to be formatted to standard output
+ * @...: Optional arguments
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, j = 0, res = 0, count = 0;
+	flags flag;
+
+	if (!format)
+		return (-1);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			j = i + 1;
+			while (flag(format[j], &flag))
+				j++;
+			res += get_print_func(format[j])(va_args(args), flag);
+			if (!res)
+				return (-1);
+			i = j;
+			count += res;
+		}
+		else
+		{
+			count += _putchar(format[i])
+		}
+		i++;
+	}
+
+	return (count);
+}
---
+++ test/README.md
@@ -0,0 +1,1 @@
+This folder contains files that will be used to drive/test our functions using main.c to avoid having main.c files in same directory with other c files.
