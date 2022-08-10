da313be7057bc65583b1b07dd2aaab4034f6246b
Pelumiii1 <adesokanpelumi14@gmail.com>
2022-08-10 16:52:09 +0100

    edited main file

--- 0-main.c
+++ 0-main.c
@@ -18,6 +18,7 @@ int main(void)
 	printf("Negative: [%d, %i]\n", len2, len2);
 	_printf("Character: [%c]\n", 'H');
 	printf("Character: [%c]\n", 'H');
+	printf("Character: [%c]\n", 'H');
 	len = _printf("String: [%s]\n", "I am a string !");
 	len2 = printf("String: [%s]\n", "I am a string !");
 	_printf("Len:[%d]\n", len);
